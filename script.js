afed345303bfa039b76deced91b0193621fdf731
AndyHuynh06 <96400179+AndyHuynh06@users.noreply.github.com>
2023-07-25 16:16:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -23,24 +23,3 @@ function opentab(tabname) {
     document.getElementById(tabname).classList.add("active-tab");
 }
 
-
-var chatbox = document.getElementById('fb-customer-chat');
- chatbox.setAttribute("page_id", "101465626374786");
-chatbox.setAttribute("attribution", "biz_inbox");
-
-
-
-window.fbAsyncInit = function() {
-    FB.init({
-        xfbml            : true,
-        version          : 'v17.0'
-        });
-};
-
-(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
